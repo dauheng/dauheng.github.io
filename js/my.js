d09bb9400d08b3af97dfb411c0d1fe426f715f1c
dauheng <dauheng@dauheng-2.local>
2014-10-02 22:32:14 +0800

    add test

--- js/my.js
+++ js/my.js
@@ -1,3 +1,10 @@
 ;(function(window, document, undefined){
+	document.querySelector('.box').addEventListener('click', function(e) {
+		console.log([this, 'click on box']);
+		document.querySelector('.red').classList.add('red-top');
+	}, false);
 
+	document.querySelector('.red').addEventListener('click', function(e) {
+		this.classList.remove('red-top');
+	}, false);
 })(window, document);
